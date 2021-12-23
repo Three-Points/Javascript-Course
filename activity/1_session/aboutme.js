6ab9e716d8bd99281e3da276f15c1a1a2ec713df
Emmanuel Rodríguez <58270359+roremdev@users.noreply.github.com>
2021-12-22 23:02:35 -0600

    feat: ✨ added aboutme.js file

---
+++ activity/1_session/aboutme.js
@@ -0,0 +1,9 @@
+const fullName = 'Emmanuel Rodríguez Ramírez'
+const profession = 'Computational Engineer'
+const rol = 'Full Stack Developer'
+
+const str = `Hola, me llamo ${fullName} y me dedico a ${profession}. Estoy cursando este Máster porque me gustaría trabajar en ${rol}.`
+
+console.log(str)
+
+module.exports

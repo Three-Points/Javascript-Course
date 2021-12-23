0c3d05eab897959a9b22b992852e141d7e7a5648
Emmanuel Rodríguez <58270359+roremdev@users.noreply.github.com>
2021-12-22 22:46:07 -0600

    refactor: ♻️ added example 1

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,5 +1,13 @@
 // Ejercicio 1
 
+const fullName = 'Emmanuel Rodríguez Ramírez'
+const profession = 'Computational Engineer'
+const rol = 'Full Stack Developer'
+
+const str = `Hola, me llamo ${fullName} y me dedico a ${profession}. Estoy cursando este Máster porque me gustaría trabajar en ${rol}.`
+
+console.log(str)
+
 console.log("Ejercicio 1");
 
 // Ejercicio 2

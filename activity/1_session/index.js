70f66d92431bf4d95f0e0612a153fb273ed15257
Emmanuel Rodríguez <58270359+roremdev@users.noreply.github.com>
2021-12-22 23:02:13 -0600

    refactor: ♻️ change definition for breakpoints

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,12 +1,6 @@
 // Ejercicio 1
 
-const fullName = 'Emmanuel Rodríguez Ramírez'
-const profession = 'Computational Engineer'
-const rol = 'Full Stack Developer'
-
-const str = `Hola, me llamo ${fullName} y me dedico a ${profession}. Estoy cursando este Máster porque me gustaría trabajar en ${rol}.`
-
-console.log(str)
+const aboutme = require('./aboutme.js')
 
 console.log("Ejercicio 1");
 
